10163b61cfa901ab1e8920a375d7bd18525dccfb
Deepak Sharma <122921736+dipankerz@users.noreply.github.com>
2024-12-19 23:01:34 +0530

    udated the links

--- project-main/download_files.py
+++ project-main/download_files.py
@@ -3,12 +3,12 @@ assets_folder = "./assets/"
 if not os.path.exists(assets_folder):
     os.makedirs(assets_folder)
 files = {
-    "rmvpe/rmvpe.pt":"https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
-    "hubert/hubert_base.pt":"https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt",
-    "pretrained_v2/D40k.pth":"https://huggingface.co/Rejekts/project/resolve/main/D40k.pth",
-    "pretrained_v2/G40k.pth":"https://huggingface.co/Rejekts/project/resolve/main/G40k.pth",
-    "pretrained_v2/f0D40k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0D40k.pth",
-    "pretrained_v2/f0G40k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0G40k.pth"
+    "rmvpe/rmvpe.pt":"https://huggingface.co/dipankerz/rvc_0.0.0/resolve/main/rmvpe.pt",
+    "hubert/hubert_base.pt":"https://huggingface.co/dipankerz/rvc_0.0.0/resolve/main/hubert_base.pt",
+    "pretrained_v2/D48k.pth":"https://huggingface.co/dipankerz/rvc_0.0.0/resolve/main/D48k.pth",
+    "pretrained_v2/G48k.pth":"https://huggingface.co/dipankerz/rvc_0.0.0/resolve/main/G48k.pth",
+    "pretrained_v2/f0D48k.pth":"https://huggingface.co/dipankerz/rvc_0.0.0/resolve/main/f0D48k.pth",
+    "pretrained_v2/f0G48k.pth":"https://huggingface.co/dipankerz/rvc_0.0.0/resolve/main/f0G48k.pth"
 }
 for file, link in files.items():
     file_path = os.path.join(assets_folder, file)
@@ -17,3 +17,5 @@ for file, link in files.items():
             subprocess.run(['wget', link, '-O', file_path], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error downloading {file}: {e}")
+
+
